10e75159737b20211b41f9b9c04140145b55dd8d
Keon <Keon.ke@everfortune.ai>
2024-12-04 17:10:26 +0800

    🐞 fix: build lib mode

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -4,26 +4,43 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import tsConfigPaths from 'vite-tsconfig-paths'
-import * as packageJson from './package.json'
 
-
-export default defineConfig((configEnv) => ({
+export default defineConfig(() => ({
   plugins: [
     react(),
     tsConfigPaths(),
     dts({
-      include: ['src/components/'],
+      include: ['lib'],
+      rollupTypes: true,
     }),
   ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
   build: {
+    copyPublicDir: false,
     lib: {
-      entry: resolve('src', 'components/index.ts'),
-      name: 'Components',
-      formats: ['es', 'umd'],
-      fileName: (format) => `components.${format}.js`,
+      entry: {
+        main: resolve(__dirname, 'lib/main.ts')
+      },
+      name: 'MyLib',
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format === "es" ? "mjs" : "cjs"}`,
     },
     rollupOptions: {
-      external: [...Object.keys(packageJson.peerDependencies)],
+      input: {
+        main: resolve(__dirname, 'lib/main.ts'),
+      },
+      external: ["react", "react-dom", 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      }
     },
+    emptyOutDir: true,
   },
 }))
